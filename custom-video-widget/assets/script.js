6f8294a1e24f686e44264a34e3d68354703238bb
luongha <luong@gmail.com>
2025-03-20 16:21:34 +0700

    Update CSS for Feature Video

--- custom-video-widget/assets/script.js
+++ custom-video-widget/assets/script.js
@@ -53,23 +53,13 @@ document.addEventListener("DOMContentLoaded", function () {
       var swiper = new Swiper(swiperContainer, {
         slidesPerView: 3,
         slidesPerGroup: 1,
-        spaceBetween: 0,
-        loopAdditionalSlides: 3, // Đảm bảo Swiper không tạo quá nhiều bản sao
+        loop: false,
+        centeredSlides: false,
         navigation: {
           nextEl: swiperContainer.querySelector(".swiper-button-next"),
           prevEl: swiperContainer.querySelector(".swiper-button-prev"),
         },
         on: {
-          slideChangeTransitionEnd: function () {
-            console.log("Active Index:", this.activeIndex);
-            console.log("TranslateX:", this.translate);
-
-            // Ép translateX về đúng vị trí, tránh bị lệch
-            let expectedTranslateX =
-              -this.activeIndex * this.slides[0].offsetWidth;
-            this.setTranslate(expectedTranslateX);
-            console.log("Fixed TranslateX:", expectedTranslateX);
-          },
           init: function () {
             updateSlideVisibility(this);
           },
--- custom-video-widget/custom-video-widget.php
+++ custom-video-widget/custom-video-widget.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Custom Video Widget
  * Description: Widget video tùy chỉnh cho Elementor.
- * Version: 2.2
+ * Version: 2.4
  * Author: Your Name
  */
 
